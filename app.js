4f8f61757d98b69c12661e6014447ee36de9a956
Robert Greenstreet <robertgreenstreet@gmail.com>
2020-03-02 15:48:44 -0600

    header and footer partials set up and bootstrap installed

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +36,14 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+let port = process.env.PORT;
+if (port == null || port == "" || typeof port === 'undefined') {
+  port = 8080;
+}
+app.listen(port, () => {
+	console.log(`Server has started, listening on port ${port}`);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('getting home page');
   res.render('index', { title: 'Robert Greenstreet - Home' });
 });
 
